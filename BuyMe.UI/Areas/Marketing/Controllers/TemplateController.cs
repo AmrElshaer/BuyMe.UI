1c940085b9c42b8df0e109481870deff49d35893
Amr Elshaer <amrelsher07@gmail.com>
2021-07-29 11:49:50 +0200

    update

--- BuyMe.UI/Areas/Marketing/Controllers/TemplateController.cs
+++ BuyMe.UI/Areas/Marketing/Controllers/TemplateController.cs
@@ -1,7 +1,9 @@
 ﻿using BuyMe.Application.Company.Commonds.UpdateTemplate;
 using BuyMe.Application.Company.Queries;
 using BuyMe.Application.Template.Queries;
+using BuyMe.UI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,17 @@ namespace BuyMe.UI.Areas.Marketing.Controllers
 {
     public class TemplateController : BaseController
     {
+        private readonly MarketingSettings _options;
+        public TemplateController(IOptions<MarketingSettings> options)
+        {
+            _options = options.Value;
+        }
         public async Task<IActionResult> Index()
         {
             var templates =await Mediator.Send(new GetTempWithImagesQuery());
-            ViewBag.CompanyTemplate = (await Mediator.Send(new GetCompanyQuery()))?.TemplateId;
+            var company = await Mediator.Send(new GetCompanyQuery());
+            ViewBag.CompanyTemplate = company?.TemplateId;
+            ViewBag.MarketingLink = $"{_options.Domain}{company.Name}";
             return View(templates);
         }
         public async Task<IActionResult> UpdateCompanyTemplate(int templateId)
