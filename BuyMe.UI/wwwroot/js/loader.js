0844356e061fe5e4791e99893fd1c5cb7a67b649
Amr Elshaer <amrelsher07@gmail.com>
2021-08-06 15:31:37 +0200

    add loader

---
+++ BuyMe.UI/wwwroot/js/loader.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    console.log("Loader script");
+    $(document).bind('ajaxStart', function () {
+        console.log("Loader start");
+        $("#loaderbody").removeClass('hidden');
+    }).bind('ajaxStop', function () {
+        console.log("Loader end");
+        $("#loaderbody").addClass('hidden');
+    });
+});
