1a2cfbc7133fd0ea1fc3d9e5f968fe79a907f1f1
Amr Elshaer <amrelsher07@gmail.com>
2021-08-13 11:04:58 +0200

    update

--- BuyMe.Application/Common/Interfaces/ICompanyService.cs
+++ BuyMe.Application/Common/Interfaces/ICompanyService.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using BuyMe.Application.Company.Queries;
+using System.Threading.Tasks;
 
 namespace BuyMe.Application.Common.Interfaces
 {
     public interface ICompanyService
     {
+        Task<CompanyDto> GetCurrentCompany();
         Task<bool> IsActive(int? companyId);
     }
 }
--- BuyMe.Application/Company/CompanyService.cs
+++ BuyMe.Application/Company/CompanyService.cs
@@ -1,4 +1,6 @@
-﻿using BuyMe.Application.Common.Interfaces;
+﻿using AutoMapper;
+using BuyMe.Application.Common.Interfaces;
+using BuyMe.Application.Company.Queries;
 using System.Threading.Tasks;
 
 namespace BuyMe.Application.Company
@@ -6,15 +8,23 @@ namespace BuyMe.Application.Company
     public class CompanyService : ICompanyService
     {
         private readonly IBuyMeDbContext _context;
+        private readonly ICurrentUserService currentUserService;
+        private readonly IMapper mapper;
 
-        public CompanyService(IBuyMeDbContext context)
+        public CompanyService(IBuyMeDbContext context,ICurrentUserService currentUserService,IMapper mapper)
         {
             this._context = context;
+            this.currentUserService = currentUserService;
+            this.mapper = mapper;
         }
 
         public async Task<bool> IsActive(int? companyId)
         {
             return companyId != null ? (await _context.Companies.FindAsync(companyId)).IsActive : true;
+        }
+        public async Task<CompanyDto> GetCurrentCompany()
+        {
+            return mapper.Map<CompanyDto>( await _context.Companies.FindAsync(currentUserService.CompanyId));
         }
     }
 }
